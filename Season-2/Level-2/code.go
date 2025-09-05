5b0b14ff5a245486013d56cb713a3f6e3d15dfb9
Yu YAGIHASHI <yagihashoooooo@gmail.com>
2025-09-05 07:36:53 +0000

    a

--- Season-2/Level-2/code.go
+++ Season-2/Level-2/code.go
@@ -58,14 +58,13 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		password := reqBody.Password
 
 		if !isValidEmail(email) {
-			log.Printf("Invalid email format: %q", email)
 			http.Error(w, "Invalid email format", http.StatusBadRequest)
 			return
 		}
 
 		storedPassword, ok := testFakeMockUsers[email]
 		if !ok {
-			http.Error(w, "invalid email or password", http.StatusUnauthorized)
+			http.Error(w, "Invalid Email or Password", http.StatusUnauthorized)
 			return
 		}
 
